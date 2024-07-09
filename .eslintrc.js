97220283211847c3b5e6ea18cb98e02838b886c5
ingeniumed <ingeniumed@users.noreply.github.com>
2024-07-09 16:11:59 +1000

    Attempting to fix via packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,26 @@
 require( '@automattic/eslint-plugin-wpvip/init' );
 
 module.exports = {
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		babelOptions: {
+			presets: [ '@wordpress/babel-preset-default', '@babel/preset-react' ],
+		},
+	},
 	extends: [ 'plugin:@automattic/wpvip/recommended' ],
 	root: true,
 	env: {
 		jest: true,
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+			},
+			webpack: {
+				config: 'webpack.config.js',
+			},
+		},
 	},
 	rules: {
 		camelcase: 0,
@@ -12,11 +28,6 @@ module.exports = {
 		'no-shadow': 0,
 		'no-unused-vars': 0,
 		'prefer-const': 0,
-	},
-	parser: '@babel/eslint-parser',
-	parserOptions: {
-		babelOptions: {
-			presets: [ '@wordpress/babel-preset-default', '@babel/preset-react' ],
-		},
+		'id-length': 0,
 	},
 };
