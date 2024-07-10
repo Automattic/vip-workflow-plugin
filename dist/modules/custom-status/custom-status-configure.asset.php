04b945a2a0931ca32dc0d31674472d27d4d1e13d
ingeniumed <ingeniumed@users.noreply.github.com>
2024-07-10 10:46:20 +1000

    Updated build

--- dist/modules/custom-status/custom-status-configure.asset.php
+++ dist/modules/custom-status/custom-status-configure.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4ef8894f08cfdcd03325');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '65d693387e193b7a7b35');
