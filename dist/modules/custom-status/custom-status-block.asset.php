36055d74be1c59eeafb67d0e3f9005d1bd71729d
Hanif Norman <hanifnorman@gmail.com>
2024-07-10 11:00:56 +0800

    Rebuild assets for minified versions

--- dist/modules/custom-status/custom-status-block.asset.php
+++ dist/modules/custom-status/custom-status-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '51c96ea4cb034d770ba6');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => 'e7b3fa00954cfbb6a62e');
