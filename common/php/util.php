2122fa7c5e802980cca91888480b424f28eafc48
Alec Geatches <alec.geatches@automattic.com>
2024-08-09 11:37:47 -0600

    Fix spacing in util.php

--- common/php/util.php
+++ common/php/util.php
@@ -37,7 +37,7 @@ endif;
 if ( ! function_exists( '_vw_wp_link_page' ) ) {
 	function _vw_wp_link_page( $i, $custom_statuses ) {
 		global $wp_rewrite;
-		$post = get_post();
+		$post       = get_post();
 		$query_args = array();
 
 		if ( 1 == $i ) {
@@ -47,7 +47,7 @@ if ( ! function_exists( '_vw_wp_link_page' ) ) {
 			// Check for all custom post statuses, not just draft & pending
 			if ( '' == get_option( 'permalink_structure' ) || in_array( $post->post_status, array_merge( $custom_statuses, array( 'pending' ) ) ) ) {
 				$url = add_query_arg( 'page', $i, get_permalink() );
-			} else if ( 'page' == get_option( 'show_on_front' ) && get_option( 'page_on_front' ) == $post->ID ) {
+			} elseif ( 'page' == get_option( 'show_on_front' ) && get_option( 'page_on_front' ) == $post->ID ) {
 				$url = trailingslashit( get_permalink() ) . user_trailingslashit( "$wp_rewrite->pagination_base/" . $i, 'single_paged' );
 			} else {
 				$url = trailingslashit( get_permalink() ) . user_trailingslashit( $i, 'single_paged' );
@@ -58,7 +58,7 @@ if ( ! function_exists( '_vw_wp_link_page' ) ) {
 
 			// Check for all custom post statuses, not just the draft
 			if ( ( ! in_array( $post->post_status, $custom_statuses ) ) && isset( $_GET['preview_id'], $_GET['preview_nonce'] ) ) {
-				$query_args['preview_id'] = wp_unslash( $_GET['preview_id'] );
+				$query_args['preview_id']    = wp_unslash( $_GET['preview_id'] );
 				$query_args['preview_nonce'] = wp_unslash( $_GET['preview_nonce'] );
 			}
 
