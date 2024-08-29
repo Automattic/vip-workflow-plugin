05e2115cd4a47b373bd778d65bc290c7fdd737ba
Gopal Krishnan <ingeniumed@users.noreply.github.com>
2024-08-29 17:16:59 +1000

    Add editorial metadata fields to the block editor (#23)
    * Show the editorial metadata on the block editor, comment out the edit functionality and ensure new post_meta are registered when a EM is created
    
    * Attempting to read and write text type for post meta
    
    * Attempting to get the meta working
    
    * Got the fields registered at last
    
    * Got the editorial metadata fields working finally, and expanded support to checkbox and date
    
    * Re-add the edit functionality but restrict type and change the format of the keys
    
    * SHow the type on the settings page
    
    * Update the README with editorial metadata
    
    * Add a note about the unsupported post type limitation
    
    * CR changes
    
    * FIx CS violations

--- modules/editorial-metadata/lib/components/editorial-metadata-manager.js
+++ modules/editorial-metadata/lib/components/editorial-metadata-manager.js
@@ -82,6 +82,11 @@ export default function EditorialMetadataManager( {
 		}
 	};
 
+	// Convert metadata type to a label, that's in sentence case
+	const convertMetadataTypeToLabel = metadataType => {
+		return metadataType.charAt( 0 ).toUpperCase() + metadataType.slice( 1 );
+	};
+
 	const deleteModal = (
 		<ConfirmDeleteModal
 			confirmationMessage={ '' }
@@ -96,7 +101,7 @@ export default function EditorialMetadataManager( {
 			availableMetadataTypes={ availableMetadataTypes.map( availableMetadataType => {
 				return {
 					value: availableMetadataType,
-					label: availableMetadataType,
+					label: convertMetadataTypeToLabel( availableMetadataType ),
 				};
 			} ) }
 			metadata={ eMetadataTerm }
@@ -131,6 +136,7 @@ export default function EditorialMetadataManager( {
 											<Text>
 												<i>{ eMetadataTerm.description }</i>
 											</Text>
+											<Text>{ convertMetadataTypeToLabel( eMetadataTerm.type ) }</Text>
 										</Flex>
 									</CardHeader>
 									<CardFooter>
---
+++ modules/editorial-metadata/lib/editorial-metadata-block.js
@@ -0,0 +1,93 @@
+import { __experimentalText as Text, TextControl, ToggleControl } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
+import { withDispatch, withSelect } from '@wordpress/data';
+import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { __ } from '@wordpress/i18n';
+import { registerPlugin } from '@wordpress/plugins';
+import './editor.scss';
+
+const editorialMetadatas = window.VW_EDITORIAL_METADATA.editorial_metadata_terms.map(
+	editorialMetadata => ( {
+		key: editorialMetadata.meta_key,
+		label: editorialMetadata.name,
+		type: editorialMetadata.type,
+		term_id: editorialMetadata.term_id,
+		description: editorialMetadata.description,
+	} )
+);
+
+const noEditorialMetadatasToShow = editorialMetadatas.length === 0;
+
+const CustomMetaPanel = ( { metaFields, setMetaFields } ) => (
+	<PluginDocumentSettingPanel name="editorialMetadataPanel" title="Editorial Metadata">
+		{ noEditorialMetadatasToShow && (
+			<Text>
+				{ __(
+					'Configure your editorial metadata, within the VIP Workflow Plugin Settings.',
+					'vip-workflow'
+				) }
+			</Text>
+		) }
+		{ editorialMetadatas.map( editorialMetadata =>
+			getComponentByType( editorialMetadata, metaFields, setMetaFields )
+		) }
+	</PluginDocumentSettingPanel>
+);
+
+function getComponentByType( editorialMetadata, metaFields, setMetaFields ) {
+	if ( editorialMetadata.type === 'checkbox' ) {
+		return (
+			<ToggleControl
+				key={ editorialMetadata.key }
+				help={ editorialMetadata.description }
+				label={ editorialMetadata.label }
+				checked={ metaFields?.[ editorialMetadata.key ] }
+				onChange={ value =>
+					setMetaFields( {
+						...metaFields,
+						[ editorialMetadata.key ]: value,
+					} )
+				}
+			/>
+		);
+	}
+	return (
+		<TextControl
+			key={ editorialMetadata.key }
+			help={ editorialMetadata.description }
+			label={ editorialMetadata.label }
+			value={ metaFields?.[ editorialMetadata.key ] }
+			className={ editorialMetadata.key }
+			onChange={ value =>
+				setMetaFields( {
+					...metaFields,
+					[ editorialMetadata.key ]: value,
+				} )
+			}
+		/>
+	);
+}
+
+const applyWithSelect = select => {
+	return {
+		metaFields: select( 'core/editor' ).getEditedPostAttribute( 'meta' ),
+	};
+};
+
+const applyWithDispatch = dispatch => {
+	return {
+		setMetaFields( newValue ) {
+			dispatch( 'core/editor' ).editPost( { meta: newValue } );
+		},
+	};
+};
+
+const plugin = compose(
+	withSelect( applyWithSelect ),
+	withDispatch( applyWithDispatch )
+)( CustomMetaPanel );
+
+registerPlugin( 'vip-workflow-editorial-metadata', {
+	render: plugin,
+	icon: 'vip-workflow',
+} );
