eae38374e295d1d2fac913fcae875c1463eac34f
ingeniumed <ingeniumed@users.noreply.github.com>
2024-10-08 16:00:35 +1100

    Up the workflow version of the plugin

--- vip-workflow.php
+++ vip-workflow.php
@@ -5,7 +5,7 @@
  * Description: Adding additional editorial workflow capabilities to WordPress.
  * Author: WordPress VIP
  * Text Domain: vip-workflow
- * Version: 0.3.0
+ * Version: 0.4.0
  * Requires at least: 6.4
  * Requires PHP: 8.1
  * License: GPL-3
@@ -35,15 +35,16 @@ if ( version_compare( phpversion(), '8.1', '<' ) || version_compare( $wp_version
 }
 
 // Define contants
-define( 'VIP_WORKFLOW_VERSION', '0.3.0' );
+define( 'VIP_WORKFLOW_VERSION', '0.4.0' );
 define( 'VIP_WORKFLOW_ROOT', __DIR__ );
 define( 'VIP_WORKFLOW_URL', plugins_url( '/', __FILE__ ) );
 define( 'VIP_WORKFLOW_SETTINGS_PAGE', add_query_arg( 'page', 'vw-settings', get_admin_url( null, 'admin.php' ) ) );
 define( 'VIP_WORKFLOW_REST_NAMESPACE', 'vip-workflow/v1' );
 
 
-// Set the version for the plugin
+// Set the version for the plugin.
 // It's not used for anything, which is why it's here.
+// This should not rely on any other code in the plugin.
 add_action( 'admin_init', function () {
 	$previous_version = get_option( 'vip_workflow_version' );
 	if ( $previous_version && version_compare( $previous_version, VIP_WORKFLOW_VERSION, '<' ) ) {
